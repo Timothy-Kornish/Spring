be7a93e74b7e598cd4d791fad84dded4fb98fe99
Timothy Kornish <TimmehK@Timothys-MacBook-Pro.local>
2018-01-17 16:44:08 -0700

    creating uni-directional databse connection for an instructor

--- workspace/hb-01-one-to-one-uni-directional/src/com/timothyKornish/hibernate/demo/DeleteDemo.java
+++ workspace/hb-01-one-to-one-uni-directional/src/com/timothyKornish/hibernate/demo/DeleteDemo.java
@@ -4,34 +4,43 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.timothyKornish.hibernate.demo.entity.Instructor;
+import com.timothyKornish.hibernate.demo.entity.InstructorDetail;
 import com.timothyKornish.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class DeleteDemo {
 
 	public static void main(String[] args) {
 		
 		// create session factory
 		SessionFactory factory = new Configuration()
 									 .configure("hibernate.cfg.xml")
-									 .addAnnotatedClass(Student.class)
+									 .addAnnotatedClass(Instructor.class)
+									 .addAnnotatedClass(InstructorDetail.class)
 									 .buildSessionFactory();
 		// create session
 		Session session = factory.getCurrentSession();
 		
 		try {
-			// use the session object to save Java object
-			// create student object
-			System.out.println("Creating a new student object...");
-			Student tempStudent = new Student("Paul", "Wall", "Paul.wall@coding.com");
+			
 			
 			// start a transaction
 			System.out.println("Beginning transaction...");
 			session.beginTransaction();
-			
-			// save the student object
-			System.out.println("saving student in transaction...");
-			session.save(tempStudent);
-			
+						
+			// get instructor by primary key / id
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+						
+			System.out.println("Founr instructor: " + tempInstructor);
+			// delete the instructor
+			if(tempInstructor != null) {
+				System.out.println("Deleting: " + tempInstructor);
+							
+				// Note: will ALso delete associated "details" object because of CasacadeType.ALL
+				session.delete(tempInstructor);
+			}
+						
 			// commit transaction
 			session.getTransaction().commit();
 			System.out.println("transaction done!");
