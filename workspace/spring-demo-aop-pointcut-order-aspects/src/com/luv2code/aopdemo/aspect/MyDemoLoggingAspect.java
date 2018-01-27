b075a5405921b9acd3e612720488a6e5498d9197
TimmehK <Tlkornish@gmail.com>
2018-01-26 17:01:13 -0700

    setting order of precedence with pointcut aspects

--- workspace/spring-demo-aop-pointcut-order-aspects/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ workspace/spring-demo-aop-pointcut-order-aspects/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,47 +8,25 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyDemoLoggingAspect {
 	
-	// create pointcut for all methods in all classes of dao package
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
 	
-	// create pointcut for getter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-	private void forGetMethods() {}
-	
-	// create pointcut for setter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-	private void forSetMethods() {}
-	
-	// create poincut: include dao package ... exclude getter/setter methods
-	@Pointcut("forDaoPackage() && !(forGetMethods() || forSetMethods())")
-	private void forDaoPackageNoGetterSetter() {}
-	
-	
-	@Before("forDaoPackage()")
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackage()")
 	public void beforeAddAccount() {
 		System.out.println("\n================");
 		System.out.println("Executing @Before advice on all methods in dao package using pointcut");
 		System.out.println("================\n");
 	}
-	
-	@Before("forDaoPackageNoGetterSetter()")
-	public void performAPIAnalytics() {
-		System.out.println("\n================");
-		System.out.println("Performing API analytics function");
-		System.out.println("================\n");
-	}
-	
-	@Before("forGetMethods()")
+
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forGetMethods()")
 	public void performingGet() {
 		System.out.println("\n================");
 		System.out.println("Performing getter method");
 		System.out.println("================\n");
 	}
 	
-	@Before("forSetMethods()")
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forSetMethods()")
 	public void performingSet() {
 		System.out.println("\n================");
 		System.out.println("Performing setter method");
